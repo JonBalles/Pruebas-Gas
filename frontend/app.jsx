1cdb0b5bcb0777af80fb9db4db33ed515a5ddb53
OtraPersona <jonatan.a.ballestero@gmail.com>
2025-04-16 11:55:44 -0300

    WebApp funciona, ServerLive funciona

---
+++ frontend/app.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+    return(
+        <>
+        <div className="container justify-content-center text-center mt-5">
+            <h1 className="display-5">Hola soy un componente</h1>
+        </div>
+        </>
+    )
+}
+
+
+export default App;
